44de3047a4bcdc82c3f6e1baced12293a5aef886
fteng328 <fteng328@gmail.com>
2013-03-31 18:25:06 -0400

    first commit

---
+++ max_match.c
@@ -0,0 +1,14 @@
+//part A of the programming assignment - finding maximal matches
+
+
+
+
+
+
+
+// given two strings, index all the k-long strings( and their locations, and then use the k-long strings in the other to query this index.
+
+
+// whenever a match is found, we extend it in both directions to the first mismatch and return a tupple indicating positions of this maximal match in both sequences.)
+
+
