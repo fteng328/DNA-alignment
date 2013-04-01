889ff76dea9a60522dc23241b6d44d8368675aea
fteng328 <fteng328@gmail.com>
2013-04-01 04:22:21 -0400

    lastest

---
+++ max_match_linear.c
@@ -0,0 +1,23 @@
+/*this solution finds optimal alignments in linear space*/
+/* This implementation is from Hirschberg's idea*/
+/* this is essentially the LCS problem*/
+#include <stdio.h>
+
+#define M 10
+#define N 10
+#define k 5
+
+
+main(){
+
+    char string2[N-1]="123456789";//[1,2,3,4,5,6,7,8,9];
+    char string1[M-1]="abcdefghi";//=[11,12,13,123,4411,4,43,2,4,2,];
+    char tempMatrix [M-1][k-1];
+    int i=0;
+    int j=0;
+
+
+
+
+
+}
