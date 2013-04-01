5c629b5eec99d4712cc42362bc0c8a736719499a
fteng328 <fteng328@gmail.com>
2013-03-31 23:16:08 -0400

    latest

--- max_match.c
+++ max_match.c
@@ -1,9 +1,10 @@
 //part A of the programming assignment - finding maximal matches
 
+#include <stdio.h>
 
-
-
-
+#define M 10
+#define N 10
+#define k 5
 
 
 
@@ -13,3 +14,33 @@
 // whenever a match is found, we extend it in both directions to the first mismatch and return a tupple indicating positions of this maximal match in both sequences.)
 
 
+
+main(){
+
+    char string2[N-1]="123456789";//[1,2,3,4,5,6,7,8,9];
+    char string1[M-1]="abcdefghi";//=[11,12,13,123,4411,4,43,2,4,2,];
+    char tempMatrix [M-1][k-1];
+    int i=0;
+    int j=0;
+    
+    while (i != M-1){
+        while (j!=(k-1)){
+        tempMatrix[i][j] = string2[i+j]; 
+        printf("stroring!: i= %d, j=%d\n",i,j);
+        j++;
+        }
+        j=0;
+        i++;
+        printf("done inner loop\n");
+    }
+    //printf("done filling the matrix");
+
+    
+    for (i=0;i<M;i++){
+        for (j=0;j<k;j++)
+            printf("%c,",tempMatrix[i][j]);
+        printf("\n");
+    }
+    printf("done\n");  
+    //printf("done printing the matrix");
+}
